2c6c970418ee31e36ec5dfb81f654337bdd9bdae
Edwin Castañeda <edwincastaneda@users.noreply.github.com>
2017-08-25 15:03:58 -0600

    Update first-select-example.php

--- first-select-example.php
+++ first-select-example.php
@@ -1,1 +1,30 @@
+<?php
 
+include 'config.php';
+include 'database-class.php';
+
+$database = new Database();
+$database->beginTransaction();
+
+// Consulta con/sin parametro
+if(isset($_GET['nombre_parametro'])){
+	$database->query('SELECT * FROM tabla WHERE parametro="'.$_GET['nombre_parametro'].'"');
+}else{
+	$database->query('SELECT * FROM tabla');
+}
+
+//Extraer los resultados
+$rows  = $database->resultset();
+
+//Contar los resultados
+$count = $database->rowCount();
+
+if ( $count > 0 ) {
+  
+  // impresion de los resultados
+		foreach ( $rows as $r ) {
+      echo $r["campo1"];
+      echo $r["campo2"];
+    }
+}
+$database->endTransaction();
